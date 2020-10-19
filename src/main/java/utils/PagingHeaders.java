602d11dcf8b0ae3da0ed925046639b29087c6885
francisco.acevedo <francisco.acevedo@FAcevedo.PERCEPTIOSAS.LOCAL>
2020-10-18 23:44:23 -0500

    Ajuste Pagin

--- src/main/java/utils/PagingHeaders.java
+++ src/main/java/utils/PagingHeaders.java
@@ -1,35 +1,23 @@
 package utils;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@AllArgsConstructor
+
 public enum PagingHeaders {
-	@Getter
-	@Setter
     PAGE_SIZE("Page-Size"),
-    @Getter
-    @Setter
     PAGE_NUMBER("Page-Number"),
-    @Getter
-    @Setter
     PAGE_OFFSET("Page-Offset"),
-    @Getter
-    @Setter
     PAGE_TOTAL("Page-Total"),
-    @Getter
-    @Setter
     COUNT("Count");
 
     PagingHeaders(String string) {
 		this.name = string;
 		// TODO Auto-generated constructor stub
 	}
-    @Getter
-    @Setter
-	private final String name;
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
+	
 	public String getName() {
 		return name;
 	}
