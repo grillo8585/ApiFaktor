1d7e0d249895a162bdaecbf927ee86ce5b2c3626
francisco.acevedo <francisco.acevedo@FAcevedo.PERCEPTIOSAS.LOCAL>
2020-11-05 21:05:32 -0500

    Adición de Token

--- src/main/java/com/asofi/servrest/ServrestApplication.java
+++ src/main/java/com/asofi/servrest/ServrestApplication.java
@@ -7,7 +7,21 @@ import org.apache.catalina.Manager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.asofi.servrest.config.JWTAuthorizationFilter;
 
 
 @SpringBootApplication
@@ -18,6 +32,20 @@ public class ServrestApplication {
 		
 	    SpringApplication.run(ServrestApplication.class, args);	  
 	    
+	}
+	
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/user").permitAll()
+				.anyRequest().authenticated();
+		}
 	}
 
 }
